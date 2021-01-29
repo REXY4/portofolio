c7b11afae6d35ebd279c7c09aab0bdf681d3e3b2
rexy4 <muhammadrzki96@gmail.com>
2021-01-29 23:39:25 +0700

    firstcommit

---
+++ index.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>RIZK || Portofolio</title>
+    <!--css to manual style-->
+      <link rel="stylesheet" href="style/style.css">
+
+      <style media="screen">
+
+      .deskripsi{
+        text-align: center;
+        margin-top: 200px;
+        padding-top: 10px;
+        width: 400px;
+        height: 100px;
+        background: rgb(245, 245, 245);
+      color:rgb(12, 142, 164);
+      box-shadow: 0 0 10px rgb(255, 255, 255),0 0 40px ,0 0 10px  rgb(255, 255, 255) ;
+      border-radius: 20px
+      }
+
+      .jhon{
+        margin-bottom: 10px ;
+        display: flex;
+        justify-content: center;
+      }
+
+      @media screen and (max-width:1000px){
+
+      #desk{
+          margin-top:20px;
+      }
+
+
+      }
+
+      </style>
+  </head>
+  <body >
+
+
+      <header>
+        <div class="lampHeader">
+
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+          <div class="container">
+            <a class="navbar-brand" href="#"><img src="img/drawing.svg" width="50px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  <a class="nav-link" aria-current="page" href="#">Project</a>
+                  <a class="nav-link" aria-current="page" href="#">Contact</a>
+
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div class="lampHeader">
+
+        </div>
+
+
+      </header>
+
+      <div class="container" >
+        <div class="row mt-5">
+          <div class="col sm-6">
+            <div class="jhon">
+              <img class="gw" src="img/gw.jpg" style="border-radius :50%; width:100px;">
+            </div>
+            <div class="nama">
+              <h1>Hallo,I'm Rizki</h1>
+              <p>I am a Web Developer</p>
+
+            </div>
+
+            <div class="nama">
+                 <p>I'm from indonesia</p>
+
+                 <p>i live in bogor</p>
+            </div>
+          </div>
+          <div class="col sm-6">
+            <div id="desk" class="deskripsi">
+              <h3>I have a diverse set of skill</h3>
+              <p>
+                HTML,JavaScript,CSS,jquery,botstrap,php <br>
+                and mysql
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <!--garis-->
+        <div class="row mt-5">
+          <div class="garis">
+
+          </div>
+        </div>
+
+        <div class="row mt-3">
+
+              <h1 class="h1">Project</h1>
+
+
+            <div class="col">
+              <div class="card" style="width: 18rem;">
+<img src="img/sepatu2.jpeg" class="card-img-top" alt="...">
+<div class="card-body">
+<h5 class="card-title">title</h5>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<a href="#" class="btn btn-primary">vew</a>
+</div>
+</div>
+        </div>
+
+
+        <!--garis-->
+        <div class="row mt-5">
+          <div class="garis">
+
+          </div>
+        </div>
+
+
+        <div class="row mt-3">
+
+              <h1 class="h1">Contact</h1>
+
+
+        </div>
+
+
+        <!--garis-->
+        <div class="row mt-5">
+          <div class="garis">
+
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+    <script src="js/jquery.js" ></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+
+    <script src="js/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+  </body>
+</html>
